b5f523dd54a8d619ae0f9531b13c883ffcf8d376
Paula <paulasalinor@gmail.com>
2022-06-17 00:12:46 -0300

    first steps with react-three/fiber

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useTexture } from "@react-three/drei"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Animation = () => {
+  const ref = useRef();
+
+  const [active, setActive] = useState(false)
+
+  const props = useTexture({
+    map: 'material_map.jpeg',
+    displacementMap: 'material_displacementMap.png',
+    normalMap: 'material_normalMap.jpeg',
+    roughnessMap: 'material_metallicRoughness.png',
+    aoMap: 'material_aoMap.png'
+  })
+
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime();
+    ref.current.rotation.y = a;
+  });
+
+  return (
+    <mesh scale={active ? 1.5 : 1} ref={ref} onPointerOver={() => setActive(!active)}>
+      <sphereGeometry args={[-1, 32, 32]} />
+      <meshStandardMaterial displacementScale={0.2} {...props} />    
+    </mesh>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <Canvas>
+      <ambientLight intensity={0.01} />
+      <directionalLight color="light_grey" position={[0, -1, 2]} />
+      <Animation />
+  </Canvas>
+)
